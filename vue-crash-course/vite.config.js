a4fbbaf4ca3aef0ec994210cc7b664ec22ecaaa0
Carl Fink <cafink@gmail.com>
2025-03-02 16:33:44 -0600

    Use proxy

--- vue-crash-course/vite.config.js
+++ vue-crash-course/vite.config.js
@@ -13,6 +13,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
     watch: {
       usePolling: true
     }
