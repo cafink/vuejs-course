4e360026c41b6bdebde7a0a9de345128684831c0
Carl Fink <cafink@gmail.com>
2025-03-02 16:33:34 -0600

    Add back button

--- vue-crash-course/src/views/JobView.vue
+++ vue-crash-course/src/views/JobView.vue
@@ -2,6 +2,7 @@
 	import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 	import { reactive, onMounted } from 'vue';
 	import { useRoute, RouterLink } from 'vue-router';
+	import BackButton from '@/components/BackButton.vue';
 	import axios from 'axios';
 
 	const route = useRoute();
@@ -27,6 +28,7 @@
 </script>
 
 <template>
+	<BackButton />
 	<section v-if="!state.isLoading" class="bg-green-50">
 		<div class="container m-auto py-10 px-6">
 			<div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
